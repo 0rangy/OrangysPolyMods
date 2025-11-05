7543edd853ccd2009b82358caceb31236e08d9c4
Orangy <orangywastaken@gmail.com>
2025-11-05 12:34:11 -0500

    Actually update noitalics

--- noitalics/1.1.0/main.mod.js
+++ noitalics/1.1.0/main.mod.js
@@ -1,7 +1,7 @@
 import { PolyMod, MixinType } from "https://pml.crjakob.com/PolyTrackMods/PolyModLoader/0.5.2/PolyModLoader.js";
 
 class PolyProxyMod extends PolyMod {
-    init = (pml) => {
+    preInit = (pml) => {
         pml.registerGlobalMixin(MixinType.REPLACEBETWEEN, `font-style: italic;`, `font-style: italic;`, `font-style: normal;`);
     }
 }
