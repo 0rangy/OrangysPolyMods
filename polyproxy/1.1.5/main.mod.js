8e15c419a91114990195a91b3b4c9f8b0b080a00
0rangy <86059513+0rangy@users.noreply.github.com>
2026-02-21 14:32:59 -0500

    wkklasnd

---
+++ polyproxy/1.1.5/main.mod.js
@@ -0,0 +1,11 @@
+import { PolyMod, MixinType } from "https://cdn.polymodloader.com/cb/PolyTrackMods/PolyModLoader/0.6.0-beta3/PolyTypes.js";
+
+class PolyProxyMod extends PolyMod {
+    preInit = (pml) => {
+        // this.url = "https://polyproxy.orangywastaken.workers.dev/";
+        this.url = "https://polyproxy.orangywastaken.workers.dev/";
+        pml.registerGlobalMixin(MixinType.REPLACEBETWEEN, `https://vps.kodub.com:43274/`, `https://vps.kodub.com:43274/`, this.url);
+    }
+}
+
+export let polyMod = new PolyProxyMod();
